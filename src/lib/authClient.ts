e30dd9a66b25225ebb48ac6c0c4f33e9641122ce
csodhuu <csodhuu@gmail.com>
2025-10-15 14:15:34 +0800

    base url change

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 import { getCookie } from "cookies-next";
 
-export const BASEURL = "https://blog-web-backend-kn6s.onrender.com/api/";
+export const BASEURL = "https://blog-web-backend-sand.vercel.app//api/";
 
 export const service = axios.create({
-  baseURL: "https://blog-web-backend-kn6s.onrender.com/api/",
+  baseURL: "https://blog-web-backend-sand.vercel.app/api/",
 });
 
 const isAuthRoute = (pathname: string) =>
